429481eb8a0a1487b5e41846fbb9fe1c08dffe45
abhinavsankhi <abhinavsankhi@gmail.com>
2026-04-30 14:03:51 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,18 @@
-const CACHE = "study-tracker-v3";
-const FILES = ["./", "./index.html", "./manifest.json"];
+const CACHE_NAME = 'examprep-v1';
+const ASSETS = [
+  'index.html',
+  'https://fonts.googleapis.com/css2?family=Syne:wght@700;800&family=DM+Sans:wght@400;500;600&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js'
+];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", e => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match("./index.html")))
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
